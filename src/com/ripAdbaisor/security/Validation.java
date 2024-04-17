76796776461da5c893dfc12cdbbd049fbb0d1544
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-04-17 20:27:06 +0100

    Clase validación(extiende de Restaurant): atributos(LOCATIONLIST[], userInput), constructor parametrizado de clase restaurante, getter y setter (provisionales de 'userInput'), método validar localización

---
+++ src/com/ripAdbaisor/security/Validation.java
@@ -0,0 +1,17 @@
+package com.ripAdbaisor.security;
+
+public class Validation {
+    protected int userInput;
+
+    public int getUserInput() {
+        return userInput;
+    }
+
+    public void setUserInput(int userInput) {
+        this.userInput = userInput;
+    }
+
+    public void ValidateName(){
+        
+    }
+}
